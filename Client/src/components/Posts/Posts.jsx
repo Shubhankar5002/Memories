63448e981dc6909b8257ba47fbe4b27e825252ab
Sahil Kumar <sahilku601@gmail.com>
2023-08-16 02:57:24 +0530

    -HomePage Desgin -Minor Fixes

--- Client/src/components/Posts/Posts.jsx
+++ Client/src/components/Posts/Posts.jsx
@@ -1,16 +1,24 @@
 import { useSelector } from 'react-redux';
-import Post from './Post/Post'
+import {  Grid,CircularProgress } from '@material-ui/core';
+import Post from './Post/Post';
 import useStyles from './Style.jsx'
 const Posts =function (){
     const classes=useStyles();
     const posts = useSelector((state)=>state.posts);
     console.log(posts);
     return(
-        <>
-        <h1>Posts</h1>
-        <Post/>
-        <Post/>
-        </>
+        !posts.length ? <CircularProgress/>:(
+            <Grid className={classes.container} container spacing={3} alignItems='stretch'>
+                {
+                    posts.map((post)=>(
+                        <Grid  key={post._id} item xs={12} sm={6}>
+                            <Post post={post}/>
+                        </Grid>
+                    ))
+                }
+
+            </Grid>
+        )
     )
 }
 export default Posts;
