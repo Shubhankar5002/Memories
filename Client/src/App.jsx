d6b38e70d7c7aaf4c5873d7f4654f0cdf43f781c
Shubhankar5002 <100302996+Shubhankar5002@users.noreply.github.com>
2023-08-25 00:22:25 +0530

    -Added React-react-dom

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,7 +4,7 @@ import { getPost } from './actions/posts';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
-import useStyles from './styles.jsx'
+import useStyles from './styles.jsx';
 import { useDispatch } from "react-redux";
 import './index.css'
 import Navbar from "./components/Navbar/Navbar";
